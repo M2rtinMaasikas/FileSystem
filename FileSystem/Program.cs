309fcd0a6f94ec45f0b25f88b7d0c71049f3fa9d
Opilane TTHK <opilane@tthk.ee>
2021-10-26 11:26:47 +0300

    1

---
+++ FileSystem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string rootDirectory = @"C:\Users\opilane\samples";
+            Console.WriteLine("Enter directory name:");
+            string userFolder = Console.ReadLine();
+
+            string NewDirectoryFullPath = @$"{rootDirectory}\{userFolder}";
+            bool directoryExist = Directory.Exists(NewDirectoryFullPath);
+            if (directoryExist)
+            {
+                Console.WriteLine($"{userFolder} exist");
+                Console.WriteLine("Would you like to crate a folder? Y/N");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+                if(userInput == 'y')
+                {
+                    Console.WriteLine("Enter the file name:");
+                    string userFile = Console.ReadLine() + ".txt";
+                    string userFileFullpath = $@"{NewDirectoryFullPath}\{userFile}";
+                       
+                }
+
+            } else
+            
+            {
+                Directory.CreateDirectory(NewDirectoryFullPath);
+            }
+
+            Directory.CreateDirectory(NewDirectoryFullPath);
+            Console.WriteLine($"Directory {userFolder} has been created.");
+        }
+    }
+}
